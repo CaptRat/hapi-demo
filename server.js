db035d3629f0627bfd0b7b37ef5b397a8b962203
Unknown <curt8769@gmail.com>
2017-02-14 16:11:55 -0700

    hapi video

---
+++ server.js
@@ -0,0 +1,44 @@
+var hapi = require('hapi');
+
+var server = new hapi.Server();
+
+server.connection({ port:3000 });
+
+server.ext('onRequest', function(request, reply) {
+    console.log('Request received: ' + request.path);
+    reply.continue();
+});
+
+// server.route({
+//     path: '/hello',
+//     method: 'GET',
+//     handler: function(request, reply) {
+//         reply('Hello World');
+//     }
+// });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        file: 'templates/index.html'
+    }
+});
+
+server.route({
+    path:'/assets/{path*}',
+    method:'GET',
+    handler: {
+        directory: {
+            path: './public',
+            listing: false,
+        }
+
+    }
+})
+        
+  server.start(function() {
+      console.log('Listening on ' + server.info.uri);
+  }); 
+
+
